0de5cd89edac4b6588a7bea374d59b6b535745cd
Navin <97350894+N47IN@users.noreply.github.com>
2023-10-17 20:02:21 +0530

    Create OptimPlannerV1.py

---
+++ src/Python with open3D/OptimPlannerV1.py
@@ -0,0 +1,224 @@
+import open3d as o3d
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from itertools import combinations
+from scipy.optimize import minimize
+
+
+
+class logger():
+    def __init__(self):
+      
+        self.candidate1 = []
+        self.candidate2 = []
+        self.candidate3 = []
+        self.err = []
+        self.handle_force = []
+        self.sec_force = []
+        self.ter_force = []
+    
+    def log(self,sol,id,err):
+        self.candidate1.append(id[0])
+        self.candidate2.append(id[1])
+        self.candidate3.append(id[2])
+        self.handle_force.append([sol[2]])
+        self.sec_force.append([sol[5]]) 
+        self.ter_force.append([sol[8]])
+        self.err.append(err)  
+
+    def save_file(self):
+       df = pd.DataFrame({"Pt1" : self.candidate1, "Pt2" : self.candidate2,"Pt3" : self.candidate3, "F1" : self.handle_force, "F2": self.sec_force, "F3": self.ter_force, "Error" :self.err})
+       df.to_csv("realtime_diagnostics.csv", index = False)
+       
+
+    def cost_visualizer(self):
+        k = []
+        for i in range(len(self.err)):
+            k.append(i)
+        plt.plot( k ,self.err,label=' Cost comparison')
+        plt.xlabel('Candidate Number')
+        plt.ylabel('Final cost')
+        plt.title('cost analysis')
+        plt.show()
+class KdTree:
+    def __init__(self, pcd):
+        self.pcd = pcd
+        self.kd_tree = o3d.geometry.KDTreeFlann(self.pcd)
+        self.radius_sweep = 20
+        self.selected_points = []
+        self.checked_points = []   
+
+    def get_points(self, center_point):
+
+        [k, idx, _] = self.kd_tree.search_radius_vector_3d(
+            center_point, self.radius_sweep)
+        return idx
+
+    def search(self):
+        min = 12
+        handle_id = 89
+        query_point = self.pcd.points[handle_id]
+        idx = self.get_points(query_point)
+        idx = list(idx)
+        idx.remove(handle_id)
+        force_optimization = Optimization(handle_id,idx,self.pcd)
+        force_optimization.transformation()
+        # So reqd_combination will have the required points and normals
+
+
+class Optimization:
+
+    def __init__(self,handle_id, idx, pcd):
+        self.handle_id = handle_id
+        self.idx = idx
+        self.pcd = pcd
+        self.max_force = 10
+        self.f_ext = np.asarray([0, 0, 10, 0, 0, 0])
+        # For point contact with friction
+        self.Bci = np.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1], [
+            0, 0, 0], [0, 0, 0], [0, 0, 0]])
+        self.G = None
+        self.mew = 1  # assuming it is high friction surface for testing
+        self.fc = [0, 0, 2, 0, 0, 2, 0, 0, 2]
+        self.min = 15  # Taking 15 since max error will be 10
+        self.solved_combination = None
+        self.solution = None
+   
+    def choose(self):
+
+        unique_combinations = np.asarray(
+            list(combinations(self.idx, 2)))
+        return unique_combinations
+
+    def transformation(self):
+     for k in range(len(self.pcd.points)):
+     
+        unique_combinations = list(self.choose())
+        new_combinations = [[item[0], item[1], k] for item in unique_combinations]
+         
+        for i in range(len(new_combinations)):
+            
+            self.G = None
+            self.idt = []
+            for j in range(3):
+                id = new_combinations[i][j]
+                self.idt.append(id)
+                 
+                normal = self.pcd.normals[id]
+                point = self.pcd.points[id]
+                # This gives us orientation of normal vector with x,y and z axis
+                normal = -normal
+                x_axis_angle = np.arctan2(np.linalg.norm(np.cross(
+                    normal, np.asarray([1, 0, 0]))), np.dot(normal, np.asarray([1, 0, 0])))
+                y_axis_angle = np.arctan2(np.linalg.norm(np.cross(
+                    normal, np.asarray([0, 1, 0]))), np.dot(normal, np.asarray([0, 1, 0])))
+                z_axis_angle = np.arctan2(np.linalg.norm(np.cross(
+                    normal, np.asarray([0, 0, 1]))), np.dot(normal, np.asarray([0, 0, 1])))
+
+                R_alpha = np.array([[np.cos(z_axis_angle), -np.sin(z_axis_angle), 0],
+                                    [np.sin(z_axis_angle), np.cos(
+                                        z_axis_angle), 0],
+                                    [0, 0, 1]])
+
+                R_beta = np.array([[np.cos(y_axis_angle), 0, np.sin(y_axis_angle)],
+                                   [0, 1, 0],
+                                   [-np.sin(y_axis_angle), 0, np.cos(y_axis_angle)]])
+
+                R_gamma = np.array([[1, 0, 0],
+                                    [0, np.cos(x_axis_angle), -
+                                     np.sin(x_axis_angle)],
+                                    [0, np.sin(x_axis_angle), np.cos(x_axis_angle)]])
+                R = np.dot(R_alpha, np.dot(R_beta, R_gamma))
+
+                H = np.matrix(
+                    [[0, -point[2], point[1]], [point[2], 0, -point[0]], [-point[1], point[0], 0]])
+
+                cross_G = np.dot(H, R)
+                zeros_matrix = np.zeros((3, 3))
+                G_i = np.vstack((np.hstack((R, zeros_matrix)),
+                                np.hstack((np.cross(cross_G, R), R))))
+                F_oi = np.dot(G_i, self.Bci)
+                if self.G is None:
+                    self.G = F_oi
+                else:
+                    self.G = np.hstack((self.G, F_oi))               
+            self.solve()
+
+
+    def objective_function(self, fc):
+        return np.linalg.norm(np.dot(self.G, fc)+self.f_ext)
+
+    def constraint_4(self, fc):
+        return self.mew*fc[2]-np.sqrt(fc[0]**2+fc[1]**2)
+
+    def constraint_5(self, fc):
+        return self.mew*fc[5]-np.sqrt(fc[3]**2+fc[4]**2)
+
+    def constraint_6(self, fc):
+        return self.mew*fc[8]-np.sqrt(fc[6]**2+fc[7]**2)
+
+    def solve(self):
+        con4 = {'type': 'ineq', 'fun': self.constraint_4}
+        con5 = {'type': 'ineq', 'fun': self.constraint_5}
+        con6 = {'type': 'ineq', 'fun': self.constraint_6}
+        b = (0, 10)
+        bnds = [b, b, b, b, b, b, b, b, b]
+        cons = [con4, con5, con6]
+        sol = minimize(self.objective_function, self.fc,
+                       method='SLSQP', bounds=bnds, constraints=cons)
+        err = self.objective_function(sol.x)
+        solution = list(sol.x)
+        if self.objective_function(sol.x) < 1.5:
+         log1.log(solution,self.idt,err)
+        
+def visualize(mesh):
+
+    points = np.asarray(mesh.points)
+    x_centroid = np.mean(points[:, 0])
+    y_centroid = np.mean(points[:, 1])
+    z_centroid = np.mean(points[:, 2])
+    mesh_coord_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+        size=5, origin=[x_centroid, y_centroid, z_centroid])
+    o3d.visualization.draw_geometries(
+        [mesh_coord_frame, mesh], point_show_normal=True)
+
+
+def mesh2PointCloud(mesh):
+    n_pts = 100
+    pcd = mesh.sample_points_uniformly(n_pts)
+    return pcd
+
+
+def force_visualizer(mesh, points, normals):
+    visualizer = o3d.visualization.Visualizer()
+    visualizer.create_window()
+    visualizer.add_geometry(mesh)  # Display the STL mesh
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+    pcd.normals = o3d.utility.Vector3dVector(normals)
+    visualizer.add_geometry(pcd)
+    visualizer.run()
+    visualizer.destroy_window()
+
+
+def main():
+
+    mesh_path = "cuboid.stl"
+    mesh = o3d.io.read_triangle_mesh(mesh_path)
+    mesh.compute_vertex_normals()
+    pcd = mesh2PointCloud(mesh)
+    pcd_df = pd.DataFrame(np.concatenate((np.asarray(pcd.points), np.asarray(pcd.normals)), axis=1),
+                          columns=["x", "y", "z", "norm-x", "norm-y", "norm-z"]
+                          )
+
+    obj = KdTree(pcd)
+    reqd_combination = obj.search()
+    log1.save_file()
+    log1.cost_visualizer()
+
+log1 = logger()
+
+if __name__ == "__main__":
+    main()
